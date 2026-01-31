f20e4d0d60a6415f0cc5d39b5fca42c111f5affe
lyszt <kalliddel@gmail.com>
2026-01-31 14:53:09 -0300

    repo sorting logic

--- src/resume/data/user_data.py
+++ src/resume/data/user_data.py
@@ -5,23 +5,45 @@ from src.resume import logger
 
 BLOCKED_REPOS = [
     "lyszt",
+    "lyszt.github.io",
 ]
 
-def get_repos(username: str) -> List[Dict[str, str]]:
+BLOCKED_SCHEMATICS = {
+    "_misc",
+}
 
-    github_repos = requests.get(f'https://api.github.com/users/{username}/repos').json()
+def get_repos(username: str) -> dict[int, dict[str, str]]:
+    url = f'https://api.github.com/users/{username}/repos?per_page=100'
+    github_repos = requests.get(url).json()
     repos: Dict[int, Dict[str, str]] =\
-        {r['id']: {'name': r['name'], 'desc': r['description'], 'url': r['homepage'], 'lang': r['language']}
-         for r in github_repos if not r['fork'] and not r['name'] in BLOCKED_REPOS }
+        {r['id']: {'name': r['name'], 'desc': r['description'], 'url': r['svn_url'], 'lang': r['language']}
+         for r in github_repos if not r['fork'] and not r['name'] in BLOCKED_REPOS and not r['name'].endswith(tuple(BLOCKED_SCHEMATICS))}
 
-    logger.info(repos)
-    return github_repos.json()
+    return repos
 
 
 class UserData:
     def __init__(self):
-        # Repository dictionary - Holds NAME, DESCRIPTION, LANGUAGE, URL
-        self.repos: Dict[int, Dict[str, str]]= {}
-        get_repos(username="lyszt")
+        self.user_info: Dict[int, Dict[str, str]] = get_repos('lyszt')
+        skills_set = set()
 
+        # type: List[Dict[str, str]]
+        self.core_projects = []
+        self.edu_projects = []
+        self.legacy_projects = []
 
+        for repo in self.user_info.values():
+            name = repo.get('name', '')
+
+            if lang := repo.get("lang"):
+                skills_set.add(lang)
+
+            if name.endswith("_core"):
+                self.core_projects.append(repo)
+            elif name.endswith("_edu"):
+                self.edu_projects.append(repo)
+            elif name.endswith("_legacy"):
+                self.legacy_projects.append(repo)
+
+        self.skills = sorted(list(skills_set))
+        logger.info(f"{self.skills, self.core_projects, self.edu_projects, self.legacy_projects}")
